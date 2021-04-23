1f843fb1cc3a361fd128a619a3d239cb4dd7c15b
floX <flornalix@gmail.com>
2021-04-23 09:31:07 +0200

    editiert

--- Vorlesungen/src/main/java/de/flox/vorlesungen/main/Main.java
+++ Vorlesungen/src/main/java/de/flox/vorlesungen/main/Main.java
@@ -36,31 +36,50 @@ public class Main {
                 case "a":
                     System.out.print("Gib die erste Zahl ein: ");
                     double x = inOutReader.getDouble();
+
                     System.out.print("Gib die zweite Zahl ein: ");
                     double y = inOutReader.getDouble();
+
                     System.out.println("Summe von "+x+" und "+y+": "+mathClass.addition(x, y));
                     break;
+
                 case "b":
                     System.out.print("Gib die erste Zahl ein: ");
                     double xMinus = inOutReader.getDouble();
+
                     System.out.print("Gib die zweite Zahl ein: ");
                     double yMinus = inOutReader.getDouble();
+
                     System.out.println("Subtraktion von "+xMinus+" und "+yMinus+": "+mathClass.subtrahieren(xMinus, yMinus));
                     break;
+
                 case "c":
                     System.out.print("Gib die erste Zahl ein: ");
                     double xMulti = inOutReader.getDouble();
+
                     System.out.print("Gib die zweite Zahl ein: ");
                     double yMulti = inOutReader.getDouble();
+
                     System.out.println("Multiplikation von "+yMulti+" und "+yMulti+": "+mathClass.multiplikation(yMulti, yMulti));
                     break;
+
                 case "d":
                     System.out.print("Gib die erste Zahl ein: ");
                     double xDivide = inOutReader.getDouble();
+
                     System.out.print("Gib die zweite Zahl ein: ");
                     double yDivide = inOutReader.getDouble();
+
+                    if(yDivide == 0){
+                        while(yDivide == 0){
+                            System.out.print("Gib die zweite Zahl ein die größer als 0 ist: ");
+                            yDivide = inOutReader.getDouble();
+                        }
+                    }
+
                     System.out.println("Division von "+xDivide+" und "+yDivide+": "+mathClass.division(xDivide, yDivide));
                     break;
+
                 case "x":
                     run = false;
                     break;
